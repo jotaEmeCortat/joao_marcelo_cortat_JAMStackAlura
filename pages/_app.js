9619363a2ed2d72dad5f27a05d5f87f3e00e481e
jotaEmeCortat <jmcortat@gmail.com>
2021-03-22 23:36:30 -0300

    add Head

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
+import Head from 'next/head';
 import { theme } from '../src/Theme';
 import { GlobalStyle } from '../src/Theme/GlobalStyle';
 
@@ -8,6 +9,14 @@ import { GlobalStyle } from '../src/Theme/GlobalStyle';
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>My page title</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@600;700;900&display=swap" rel="stylesheet" />
+        <link rel="icon" type="image/png" href="/img/favicon.png" />
+      </Head>
+
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
